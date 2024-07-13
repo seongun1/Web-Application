6b80bf81cb86c603d8c7b14f8cb2134644bd683b
seongun Kim <seongun44@naver.com>
2024-07-13 17:47:48 +0900

    1강 까지 완료

---
+++ src/main/java/jpabook/japshop/HelloController.java
@@ -0,0 +1,14 @@
+package jpabook.japshop;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller // 이걸 적어줘야 동적 페이지 기능 가능.
+public class HelloController {
+    @GetMapping("hello")
+    public String hello (Model model){
+        model.addAttribute("data","hello!!!");
+        return "hello";
+    }
+}
